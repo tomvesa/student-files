ac4329617c4d28c2f75e6f7d4f562150af476936
tomvesa <47905734+tomvesa@users.noreply.github.com>
2022-08-30 22:46:10 +0200

    Features -create and append modal details

--- js/scripts.js
+++ js/scripts.js
@@ -51,18 +51,71 @@ const apiConfig = (url, query={}, data=[]) =>({
                                 <p class="card-text">${email}</p>
                                 <p class="card-text cap">${city}, ${country}</p>
                             </div> `;
+          div.addEventListener("click", (e)=>{
+            displayModal(userData);
+          });                  
           gallery.appendChild(div);     
  }
 
+ function displayModal(userData){
+  let {
+    email,
+    cell,
+    picture : {medium : avatar} ,
+    name    : {first : firstName,
+                last : lastName},
+    location: {city,
+               country,
+               postcode,
+               street:{number :streetNum, 
+                        name:streetName}
+              },
+    dob : {date : dobDate}
+            } = userData;
+    const div = document.createElement("DIV");
+          div.className = "modal-container";
+          div.innerHTML = `<div class="modal">
+              <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+              <div class="modal-info-container">
+                  <img class="modal-img" src="${avatar}" alt="profile picture">
+                  <h3 id="name" class="modal-name cap">${firstName} ${lastName}</h3>
+                  <p class="modal-text">${email}</p>
+                  <p class="modal-text cap">${city}</p>
+                  <hr>
+                  <p class="modal-text">${cell}</p>
+                  <p class="modal-text"> ${streetNum} ${streetName}, ${city}, ${postcode}</p>
+                  <p class="modal-text">Birthday: ${getDobDate(dobDate)}</p>
+              </div>`;
 
+          body.appendChild(div);
+
+          closeModal();
+
+ }
+
+ function closeModal(){
+    const closeBtn = document.getElementById("modal-close-btn");
+          closeBtn.addEventListener("click", e=>{
+            closeBtn.parentElement.parentElement.remove();
+          })
+ }
+
+function getDobDate(date){
+    const dob = new Date(date);
+    let month = dob.getMonth() + 1;
+
+    return `${dob.getDate()}/${month >= 10 ? month : `0${month}` }/${dob.getFullYear()}`; 
+}
 
 
   // running the code
+  const body = document.body;
   const gallery = document.getElementById("gallery");
   let usersAPIConfig = apiConfig("https://randomuser.me/api/1.4/", 
                             {results : 12, 
-                            inc : "nat,name,location,email,picture",
+                            inc : "nat,name,location,email,picture,dob,cell",
                             nat : "us,gb,au,nz"} );
   
   makeApiCall(usersAPIConfig)
     .then(() => usersAPIConfig.data.forEach(item => createCard(item)));
+
